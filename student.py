12017ce533925d46cfdec86f4ff3a84a28d73188
Anna <CodeWithAnna@gmail.com>
2021-01-18 19:05:48 +0530

    Added first file

---
+++ student.py
@@ -0,0 +1,39 @@
+class_1=["Geoffrey Hinton","Andrew Ng","Sebastian Raschka","Yoshua Bengio"]
+class_2=["'Hilary Mason","Carla Gentry","Corinna Cortes"]
+new_class=class_1+class_2
+print(new_class)
+new_class.append("Peter Warden")
+print(new_class)
+new_class.remove("Carla Gentry")
+print(new_class)
+courses={"Math"   :65,
+         "English":70,
+         "History":80,
+         "French" :70,
+         "Science":60
+}
+print(courses)
+total = sum(courses.values())
+print(total)
+percentage=total/500*100
+print(percentage)
+mathematics={
+    "Geoffrey Hinton"  :78,
+    "Andrew Ng"        :95,
+    "Sebastian Raschka":65,
+    "Yoshua Benjio"    :50,
+    "Hilary Mason"     :70,
+    "Corinna Cortes"   :66,
+    "Peter Warden"     :75
+}
+max_mathematics_marks_scored = max(courses,key = courses.get)
+print (max_mathematics_marks_scored)
+topper=max(mathematics,key = mathematics.get)
+print(topper)
+
+data=topper.split(" ")
+first_name =print (data [0])
+last_name  =print (data [1])
+full_name = data[1]+ " "+data[0]
+certificate_name=full_name.upper()
+print(certificate_name)
